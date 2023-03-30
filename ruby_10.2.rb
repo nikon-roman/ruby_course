7805d44abe22a6cfc8fb2f534a6d89e135c2b1e5
nikon-roman <nikon@ukr.net>
2023-03-30 22:20:28 +0300

    refactor code

--- ruby_10.2.rb
+++ ruby_10.2.rb
@@ -1,26 +1,30 @@
 class Pizza
-    @@count = 0
-    
-    def initialize(d, ingredients)
-      @@count += 1
-      d == 25 || d == 50 ? @d = d : @d = 0
-      if @d == 25 || @d == 50 
-        @number = @@count
-      else
-        @number = 0
-        @@count -= 1
-      end
-      @d == 25 || @d == 50 ? @ingredients = ingredients.join(',') : @ingredients
+  @@count = 0
+  
+  def initialize(d, ingredients)
+    @@count += 1
+    d == 25 || d == 50 ? @d = d : @d = 0
+    if @d == 25 || @d == 50 
+      @number = @@count
+    else
+      @number = 0
+      @@count -= 1
     end
-    
-    def description
-      "Піца № #{ @number } (діагональ = #{@d}) містить [#{ @ingredients }]."
-    end
+    @d == 25 || @d == 50 ? @ingredients = ingredients.join(',') : @ingredients
   end
   
-  pizza1 = Pizza.new(10, ["mushroom", "cheese"])
-  pizza2 = Pizza.new 25, ["mushroom", "cheese", "cherry"]
-  pizza3 = Pizza.new 25, ["mushroom", "cheese", "cherry"]
-  pizza4 = Pizza.new(10, ["mushroom", "cheese"])
-  pizza5 = Pizza.new 25, ["mushroom", "cheese", "cherry"]
-  puts pizza4.description 
+  def description
+    "Піца № #{ @number } (діагональ = #{@d}) містить [#{ @ingredients }]."
+  end
+end
+
+pizza1 = Pizza.new(10, ["mushroom", "cheese"])
+pizza2 = Pizza.new 25, ["mushroom", "cheese", "cherry"]
+pizza3 = Pizza.new 25, ["mushroom", "cheese", "cherry"]
+pizza4 = Pizza.new(10, ["mushroom", "cheese"])
+pizza5 = Pizza.new 25, ["mushroom", "cheese", "cherry"]
+puts pizza1.description 
+puts pizza2.description 
+puts pizza3.description 
+puts pizza4.description
+puts pizza5.description 
--- ruby_10.4.rb
+++ ruby_10.4.rb
@@ -1,19 +1,19 @@
 class Pizza
-    @@count = 0
-    attr_reader :d, :number
-    attr_writer :ingredients
+  @@count = 0
+  attr_reader :d, :number
+  attr_writer :ingredients
 
-    def initialize(d, ingredients = [])
-      @@count += 1   
-      @d = d
-      @number = @@count
-      @ingredients = ingredients
-    end
-    
-    def description
-      "Піца № #{ @number } (діагональ = #{@d}) містить [#{ @ingredients.join }]."
-    end
- end
+  def initialize(d, ingredients = [])
+    @@count += 1   
+    @d = d
+    @number = @@count
+    @ingredients = ingredients
+  end
+  
+  def description
+    "Піца № #{ @number } (діагональ = #{@d}) містить [#{ @ingredients.join }]."
+  end
+end
 
 pizza1 = Pizza.new(24)
 puts pizza1.ingredients = []
